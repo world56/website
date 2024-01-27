e283966dae9f6af103d8164f84d2d7781320328b
Ming <world56@qq.com>
2024-01-27 15:12:34 +0800

    style [文本详情列表] 调整样式

--- src/components/List/Item.tsx
+++ src/components/List/Item.tsx
@@ -19,18 +19,16 @@ interface TypeListItemProps {
  * @description 凑合用吧
  */
 const Item: React.FC<TypeListItemProps> = ({ src, url, name, desc }) => (
-  <div className={styles.item}>
-    <Link href={url}>
-      <Image
-        alt="#"
-        width={280}
-        height={180}
-        src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}${src}`}
-      />
-      <h2>{name}</h2>
-      <p>{desc}</p>
-    </Link>
-  </div>
+  <Link href={url} className={styles.item}>
+    <Image
+      alt="#"
+      width={280}
+      height={180}
+      src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}${src}`}
+    />
+    <h2>{name}</h2>
+    <p>{desc}</p>
+  </Link>
 );
 
 export default Item;
