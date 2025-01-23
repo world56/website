ddfba7b8a9fe2cb4db726aaa5674e5c48c28c5ad
world56 <world56@qq.com>
2025-01-23 19:51:46 +0800

    fix: 生产环境初始化构建的错误警告

--- src/components/Navigation/Main.tsx
+++ src/components/Navigation/Main.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useMemo } from "react";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 
 const routes = [
@@ -12,30 +12,29 @@ const routes = [
   { url: "/contact", cnName: "取得联系", enName: "Contact" },
 ];
 
-function check(path: string | null, val: (typeof routes)[0]) {
-  return val.url === "/" ? path === "/" : path?.includes(val.url);
-}
-
 /**
  * @name MainNavigation 导航-个人主页
  */
 const MainNavigation = () => {
   const path = usePathname();
 
-  const location = useMemo(
-    () => routes.find((v) => check(path, v))?.enName,
-    [path],
-  );
+  const [name, setName] = useState<string>();
+
+  useEffect(() => {
+    setName(
+      routes.find((v) => v.url === `/${path?.split("/")?.at(1) || ""}`)?.enName,
+    );
+  }, [path]);
 
   return (
     <nav className="w-full h-[65px] absolute top-[5px] left-0 flex justify-between items-center select-none">
-      <h2 className="ml-6 font-bold text-2xl">{location}</h2>
+      <h2 className="ml-6 font-bold text-2xl">{name}</h2>
       <ul className="mr-6 flex w-max items-center">
         {routes.map((v) => (
           <Link key={v.url} href={v.url} draggable="false">
             <li
               className={`
-              ${v.enName === location ? "nav-select" : ""}
+              ${v.enName === name ? "nav-select" : ""}
               ml-2 py-2 px-3 font-medium rounded-full cursor-pointer
               hover:text-white hover:bg-black dark:hover:bg-white dark:hover:text-black`}
             >
