e157a968b6e351afd23469d5ce33cad243c49f15
world56 <world56@qq.com>
2025-02-12 00:39:25 +0800

    style: 调整播放功能元素

--- src/components/TextEditor/template.ts
+++ src/components/TextEditor/template.ts
@@ -1,6 +1,6 @@
 const template = {
   getVideo(url: string) {
-    return `<div class='player-media' contenteditable="false"><div class="player-video" style="width:60%;"><media-controller gesturesdisabled><video slot="media" src="${url}"></video><div slot="centered-chrome" style="border-radius: 50%;overflow: hidden;"><media-play-button></media-play-button></div><media-control-bar><media-play-button></media-play-button><media-time-range></media-time-range><media-time-display></media-time-display><media-mute-button></media-mute-button><media-volume-range></media-volume-range><media-fullscreen-button></media-fullscreen-button></media-control-bar></media-controller></div></div>`;
+    return `<div class='player-media' contenteditable="false"><div class="player-video" style="width:60%;"><media-controller gesturesdisabled><video slot="media" src="${url}"></video><div slot="centered-chrome" style="border-radius: 50%;overflow: hidden;"><media-play-button></media-play-button></div><media-control-bar><media-play-button></media-play-button><media-time-range></media-time-range><media-time-display></media-time-display><media-mute-button></media-mute-button><media-volume-range></media-volume-range></media-control-bar></media-controller></div></div>`;
   },
   getImage(url: string) {
     return `<img src='${url}' alt='#' style='width:max-content;' />`;
